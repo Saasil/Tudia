eb5dec0f28f7f9846eaebbb82716e89070160975
Saasil <feedee_@hotmail.com>
2012-07-11 01:57:22 -0300

    Pagina principal

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
-<?php
-deho "Prueba";
-?>
+<html>
+<head>
+<title>Tu dia!</title>
+</head>
+<body>
+<center><h1> Tu dia! </h1><br><br>
+<form method="post" action="comprobar.php">
+Ingresa tu nombre: <input type="text" name="nombre"><br>
+Ingresa tu contraseña: <input type="text" name="pass"><br>
+<input type="submit" value="Aceptar"><br></center>
+No tenes usuario? <a href="registrar.php">Hace click aca!</a>
+</body>
+</html>
