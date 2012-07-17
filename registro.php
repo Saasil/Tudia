b148de8c0a9193164aac567179f51c1492532218
Saasil <feedee_@hotmail.com>
2012-07-17 12:38:23 -0300

    Pidiendo los datos para almacenarlos en la base de datos

---
+++ registro.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title> Tu dia ! Registro </title>
+</head>
+<body>
+<form method="post" action="registrodatabase.php">
+Ingrese su nombre:<input type="text" name="nombrenuev"><br>
+Ingrese su contraseña:<input type="text" name="passnuev"><br>
+<input type="submit" value="Aceptar">
+
+</body>
+</html>
